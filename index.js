f3cc5dd4b51ad21fb5f183549a0c9ae5c57d4afc
wojtaz13 <krupawojciech13@gmail.com>
2020-03-30 22:36:01 +0200

    delete new user func

--- index.js
+++ index.js
@@ -12,12 +12,5 @@ bot.on('message', message => {
 bot.on('message', Help);
 bot.on('guildMemberAdd', Greetings);
 
-bot.on('message', newUser => {
-    if(newUser.content === '_add'){
-        const user = newUser.guild.addMember(Discord.GuildMember(), nick('Stachu'));
-        newUser.channel.send('user added');
-    }
-})
-
 bot.login('Njk0MTc1NzgwNjc0MjA3NzQ0.XoHzxQ.Jwwk3qk4M_eumfZqvkDGMj7dzPc');
 
